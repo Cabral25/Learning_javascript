eae21efc47768cadabafe87f310b49e5b71397ec
cabral <cabralnogueiraadenilson@gmail.com>
2025-02-20 14:54:40 -0400

    Initial commit

---
+++ basics/index.js
@@ -0,0 +1,53 @@
+//                    Javascript(20/02/2025)
+
+//console.log(`hello`);
+//console.log('I like pizza')
+
+// window.alert('This is an alert');
+
+//this is a comment
+
+/*
+    This 
+    is
+    a
+    comment
+*/
+
+document.getElementById('my-h1').textContent = 'Hello';
+document.getElementById('my-p').textContent = 'I like pizza';
+
+
+// ===> declaring a variable
+
+
+// 1. declaration of a variable          let x;
+// 2. assignment of a variable           x = 100
+// 3. declaration and assignment         let x = 100
+
+let x;
+x = 100;
+let y = 200
+
+console.log(x);  // 100
+console.log(y); // 200
+
+
+// ===> types of variables
+
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+name = 'Otto'
+
+console.log(age);
+console.log(price);
+console.log(`you are ${age}`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is ${gpa}`);
+console.log(`My name is ${name}`);
+
+console.log(typeof age); // number
+console.log(typeof price); // number
+console.log(typeof name); // string
